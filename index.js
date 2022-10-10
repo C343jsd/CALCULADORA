9c2c2eec94ed28b3e3dfee456361d03be8028924
B3 - 202 - 23 <camilo.muneraec@amigo.edu.co>
2022-10-10 11:33:37 -0500

    Primer commit de la calcualdora

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function multiplicar() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    document.getElementById("resultado").innerHTML = num1 * num2;
+}
+
